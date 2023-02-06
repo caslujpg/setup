df0a2a6cf3d2e38a9a347f5f55fffced0f6b6404
caslujpg <caslujpg@gmail.com>
2023-02-05 23:47:04 -0300

    feature: add mobo, web, back

---
+++ mobo/src/screens/home.tsx
@@ -0,0 +1,50 @@
+import { Text, View, ScrollView } from 'react-native';
+
+import { generateDatesFromYearBeginning } from '../utils/generate-dates-from-year-beginning';
+
+import { HabitDay, DAY_SIZE } from '../components/habit-day.component';
+import { Header } from '../components/header.component';
+
+const weekDays = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
+const datesFromYearStart = generateDatesFromYearBeginning();
+const minimumSummaryDatesSize = 18 * 7;
+const amountOfDaysToFill = minimumSummaryDatesSize - datesFromYearStart.length;
+
+export function Home() {
+  return (
+    <View className="flex-1	 bg-background px-8 py-16">
+      <Header />
+
+      <View className="flex-row mt-6 mb-2">
+        {weekDays.map((weekDay, i) => (
+          <Text
+            key={`${weekDay} - ${i}`}
+            className="text-white text-xl font-bold text-center mx-1"
+            style={{ width: DAY_SIZE }}
+          >
+            {weekDay}
+          </Text>
+        ))}
+      </View>
+
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        <View className="flex-row flex-wrap">
+          {datesFromYearStart.map((date) => (
+            <HabitDay key={date.toISOString()} />
+          ))}
+          {amountOfDaysToFill > 0 &&
+            Array.from({ length: amountOfDaysToFill }).map((_, index) => (
+              <View
+                key={index}
+                className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
+                style={{ width: DAY_SIZE, height: DAY_SIZE }}
+              />
+            ))}
+        </View>
+      </ScrollView>
+    </View>
+  );
+}
---
+++ web/src/components/habit-day.component.tsx
@@ -0,0 +1,11 @@
+// interface HabitDayProps {
+//   completed: number;
+// }
+
+export function HabitDay() {
+  return (
+    <div className="bg-zinc-900 w-8 h-8 text-withe rounded flex items-center justify-center">
+      <div className="w-8 h-8 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
+    </div>
+  );
+}
